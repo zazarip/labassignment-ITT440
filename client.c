82891a4d9e782f5275ebcdec506ad3dbfbce4154
zarip <johnwick12120@gmail.com>
2022-06-25 15:28:05 +0800

    lab 4

--- client.c
+++ client.c
@@ -1,38 +1,51 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>   //inet_addr
+#include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-int socket_desc;
+int socket_desc, new_socket;
 struct sockaddr_in server;
-char *message;
-
-//create socket
-socket_desc = socket(AF_INET, SOCK_STREAM,0);
-if(socket_desc == -1)
+char *message, server_reply[2000];
+//Creating a socket
+socket_desc = socket (AF_INET, SOCK_STREAM, 0);
+if (socket_desc == -1)
 {
-printf("Could not create socket");
+printf("Could not create socet");
 }
-server.sin_addr.s_addr = inet_addr("192.168.92.3"); //please enter the ip address of your server VM
-server.sin_family = AF_INET;
-server.sin_port = htons(22);
 
-//connect to remote server
-if(connect(socket_desc, (struct sockaddr*)&server, sizeof(server))<0)
+server.sin_addr.s_addr = inet_addr("192.168.92.3");
+server.sin_family = AF_INET;
+server.sin_port = htons (22);
+
+//Connecting to remote server
+if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server))< 0)
 {
-puts("connect error");
-return 1;
+	puts("Connect Error");
+	return 1;
 }
 puts("Connected\n");
-//send some data
-message = "connect";
-if(send(socket_desc, message, strlen(message),0)<0)
+//Seding data
+message = "Hi, saya danial";
+if (send(socket_desc, message, strlen(message), 0) < 0)
 {
-puts("send failed");
-return 1;
+	puts("Send Failed");
+	return 1;
 }
-puts("Data send\n");
+puts("Data Send\n");
+//Receiving a reply from the server
+if (recv(socket_desc, server_reply, 2000, 0) < 0)
+{
+	puts("recv failed");
+}
+else
+{
+	puts("Reply received \n");
+	puts(server_reply);
+}
+
+close(socket_desc);
 return 0;
 }
