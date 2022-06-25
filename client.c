4de46bf439b24831968fe1982787e0983e619fb1
zarip <johnwick12120@gmail.com>
2022-06-25 22:22:47 +0800

    lab 4

--- client.c
+++ client.c
@@ -1,51 +1,56 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <arpa/inet.h>	//inet_addr
 #include <string.h>
-#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-int socket_desc, new_socket;
-struct sockaddr_in server;
-char *message, server_reply[2000];
-//Creating a socket
-socket_desc = socket (AF_INET, SOCK_STREAM, 0);
-if (socket_desc == -1)
-{
-printf("Could not create socet");
-}
 
-server.sin_addr.s_addr = inet_addr("192.168.92.3");
-server.sin_family = AF_INET;
-server.sin_port = htons (8888);
+int main(int argc , char *argv[])
+{
+	int socket_desc;
+	struct sockaddr_in server;
+	char server_reply[2000];
+        char buffer[1024] = { 0 };
+        char message[sizeof(buffer)];
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1)
+	{
+		printf("Could not create socket");
+	}
 
-//Connecting to remote server
-if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server))< 0)
-{
-	puts("Connect Error");
-	return 1;
-}
-puts("Connected\n");
-//Seding data
-message = "Hi, saya danial";
-if (send(socket_desc, message, strlen(message), 0) < 0)
-{
-	puts("Send Failed");
-	return 1;
-}
-puts("Data Send\n");
-//Receiving a reply from the server
-if (recv(socket_desc, server_reply, 2000, 0) < 0)
-{
-	puts("recv failed");
-}
-else
-{
-	puts("Reply received \n");
+	server.sin_addr.s_addr = inet_addr("192.168.6.4"); //Please enter the ip address of your Server VM
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 8888 );
+	//Connect to remote server
+	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		puts("connect error");
+		return 1;
+	}
+
+	puts("Connected \n");
+
+       while(1){
+	//Send some data
+	printf("Client  : ");
+        scanf(" %s", message);
+
+	if( send(socket_desc , message , strlen(message) , 0) < 0)
+	{
+		puts("Send failed");
+		return 1;
+	}
+
+                    //Receive a reply from the server
+	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+	{
+		puts("recv failed");
+	}
+	printf("Server : ");
 	puts(server_reply);
-}
-
-close(socket_desc);
-return 0;
+        printf("\n");
+      }
+        close(socket_desc);
+	return 0;
 }
