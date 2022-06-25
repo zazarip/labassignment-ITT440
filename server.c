c2038f7a1325657082d2f32ea238690cb82dae3a
zarip <johnwick12120@gmail.com>
2022-06-25 17:13:03 +0800

    lab 4

--- server.c
+++ server.c
@@ -1,39 +1,51 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>  //inet_addr
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-int socket_desc,new_socket, c;
-struct sockaddr_in server, client;
+	int socket_desc, new_socket, c;
+	struct sockaddr_in server, client;
+	char *message,*client_reply;
+//Create socket	
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_desc == -1)
+	{
+		printf("Could not create socket");
+	}
+//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons (8888);
+//Bind
+	if(bind(socket_desc, (struct sockaddr*)&server, sizeof(server)) < 0 )
+	{
+		puts("Bind Failed");
+		return 1;
+	}
+puts("Bind done");
 
-//create socket
-socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-if(socket_desc == -1)
+	//Listen
+	listen(socket_desc, 3);
+	//Accept any incoming connection
+	puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+	puts("Connection accepted");
+	while ((new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)))
 {
-printf("could not crate socket");
-}
-//prepare the sockaddr_in structre
-server.sin_family =AF_INET;
-server.sin_addr.s_addr = INADDR_ANY;
-server.sin_port = htons(8888);
 
-//bind
-if(bind(socket_desc,(struct sockaddr *)&server,sizeof(server))<0)
-{
-puts("bind failed");
-}
-puts("bind done");
-
-//listen
-listen(socket_desc, 3);
-
-//accept and incoming connection
-puts("waiting for incoming connection...");
-c = sizeof(struct sockaddr_in);
-new_socket = accept(socket_desc,(struct sockaddr *)&client, (socklen_t*)&c);
-if(new_socket<0)
-{
-perror("accept failed");
-}
+		recv(new_socket, client_reply, 2000, 0);
+		puts(client_reply);
+		//Reply to the client
+		message = "Hello there, I have received your connection, zariff ;)\n";
+		write(new_socket, message, strlen(message));
+	}
+	if (new_socket < 0)
+	{
+		perror ("Accept Failed");
+		return 1;
+	}
+	return 0;
 }
