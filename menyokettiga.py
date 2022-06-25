68184d00703e339b94077c0bad52a5c313e41e00
zarip <johnwick12120@gmail.com>
2022-06-26 00:28:18 +0800

    lab 5

---
+++ menyokettiga.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+import json
+
+mydata = {"id": 55555, "name": "danny", "age": "21"}
+sendData = json.dumps(mydata)
+
+s = socket.socket()
+print("Socket successfully created")
+
+port = 8080
+
+s.bind(('', port))
+print("socket binded to " + str(port))
+
+s.listen(5)
+print("socket is listening")
+
+while True:
+        c, addr = s.accept()
+        print("Got connection from" + str(addr))
+
+        c.sendall(bytes(sendData,encoding="utf-8"))
+        buffer = c.recv(1024)
+        print(buffer)
+c.close()
